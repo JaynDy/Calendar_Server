34ab707b18ad69dc69ab8d7c0ecad0ee16e8942d
EvgeniaDark <vgndrk@gmail.com>
2025-07-24 12:30:33 +0200

    changed cors origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://calendar-9sx1ufkpr-evgenias-projects-899bbbbf.vercel.app",
+    origin: "https://calendar-ivory-eight.vercel.app",
     credentials: true,
   })
 );
